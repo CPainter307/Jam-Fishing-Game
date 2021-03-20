118ae79d3b609c87c13db8b0b2eca0296c3f460b
jlm0135 <jlm0135@auburn.edu>
2021-03-19 22:12:43 -0500

    Player Sliding Physics
    The player now slides along the boat. Once the player slides off the boat, the player is no longer attached and will fall

---
+++ Assets/Script/BoatTriggerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatTriggerController : MonoBehaviour
+{
+    public GameObject player;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            player.GetComponent<PlayerBehavior>().attached = true;
+            print("Attaching");
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            player.GetComponent<PlayerBehavior>().attached = false;
+            print("Dettaching");
+        }
+    }
+}
--- Assets/Script/PlayerBehavior.cs
+++ Assets/Script/PlayerBehavior.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    public GameObject player;
     public Rigidbody2D playerBody;
     public GameObject boat;
     // The attached variable is for when you want the player to be flung into the air
     public bool attached;
+    public float rotateSpeed = 0.03f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,20 +19,41 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // If the player is attached to the boat and not being flung into the air
         if (attached)
         {
-            // Parent the position and rotation of the player to the boat
-            player.transform.parent = boat.transform;
-            // This needs to be done, otherwise, the player starts doing some FUNKY things
-            playerBody.simulated = false;
+            playerBody.gravityScale = 1.5f;
         }
-        // Player is being flung into the air
         else
-        {   // Player now moves independently of the boat
-            player.transform.parent = null;
-            // Simulated player physics are on for the time being
-            playerBody.simulated = true;
+        {
+            playerBody.gravityScale = 1f;
         }
     }
+
+    void FixedUpdate()
+    {
+        var boatRot = boat.transform.rotation.eulerAngles.z;
+        if (attached)
+        {
+            // Boat is rotating right
+            if (boatRot >= 275 && boatRot <= 355)
+            {
+                transform.Translate(Vector2.right * boatRot * rotateSpeed * Time.deltaTime);
+            }
+            // Boat is rotating left
+            else if (boatRot >= 5 && boatRot <= 85)
+            {
+                transform.Translate(Vector2.right * (boatRot - 360) * rotateSpeed * Time.deltaTime);
+            }
+        }
+        if (attached)
+        {
+            transform.eulerAngles = new Vector3(
+                transform.eulerAngles.x,
+                transform.eulerAngles.y,
+                boat.transform.rotation.eulerAngles.z
+            );
+        }
+    }
+
+    
 }
